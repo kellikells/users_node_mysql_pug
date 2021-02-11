051ba8be15d514a2723d51ae6ba582fcf12a80c1
kelli <kellikells@gmail.com>
2021-02-11 14:13:02 -0800

    fixed app.js: app.set('view engine', 'pug'); error

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // template engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'pug');
+app.set('view engine', 'pug');
 
 
 // serving the index page
