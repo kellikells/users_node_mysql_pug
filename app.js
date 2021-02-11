ab7c9b01d163bcb4a385d01773396b2a414b6ad3
kelli <kellikells@gmail.com>
2021-02-11 12:06:35 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const { ENGINE_METHOD_ALL } = require('constants');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// for body parser 
+app.use(express.urlencoded( { extended : false }));
+
+// serve static files 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('views engine', 'pug');
+
+
+// setting up the server
+app.listen(9000, () => {
+    console.log('server running on port 9000');
+});
+
+module.exports = app;
